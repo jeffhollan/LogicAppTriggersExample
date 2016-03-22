99644bb10b971e51a86dfeaf672c728a7e72da4b
Jeff Hollan <jeffhollan@outlook.com>
2016-03-22 15:59:52 -0700

    webhook test

--- LogicAppTriggers/Controllers/WebhookTriggerController.cs
+++ LogicAppTriggers/Controllers/WebhookTriggerController.cs
@@ -20,10 +20,10 @@ namespace LogicAppTriggers.Controllers
         /// <param name="callbackUrl">URL to get from Logic Apps - @listCallbackUrl()</param>
         /// <returns></returns>
         [HttpPost, Route("api/webhooktrigger/subscribe")]
-        public HttpResponseMessage Subscribe(string callbackUrl)
+        public HttpResponseMessage Subscribe([FromBody] string callbackUrl)
         {
             subscriptions.Add(callbackUrl);
-            new Thread(() => doWork(callbackUrl)).Start();   //Start the thread of work, but continue on before it completes
+            new Thread(() => doWork()).Start();   //Start the thread of work, but continue on before it completes
             return Request.CreateResponse();
         }
 
@@ -31,11 +31,12 @@ namespace LogicAppTriggers.Controllers
         /// Some background thread monitoring when alerts should occur.  
         /// </summary>
         /// <param name="id"></param>
-        private async void doWork(string callbackUrl)
+        private async void doWork()
         {
             Task.Delay(12000).Wait(); //Do work will work for 120 seconds)
             using (HttpClient client = new HttpClient())
             {
+                foreach(string callbackUrl in subscriptions)
                 await client.PostAsync<string>(callbackUrl, @"{""Trigger"": ""Fired""}", new JsonMediaTypeFormatter(), "application/json");
             }
         }
@@ -46,7 +47,7 @@ namespace LogicAppTriggers.Controllers
         /// <param name="callbackUrl"></param>
         /// <returns></returns>
         [HttpPost, Route("api/webhooktrigger/unsubscribe")]
-        public HttpResponseMessage Unsubscribe(string callbackUrl)
+        public HttpResponseMessage Unsubscribe([FromBody] string callbackUrl)
         {
             subscriptions.Remove(callbackUrl);
             return Request.CreateResponse();
